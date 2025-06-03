0c09159d87a24150c643dbacc6b19ea81a4f2e43
Eduardo <42901658+LopezSennin@users.noreply.github.com>
2025-06-02 21:32:33 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://LopezSennin.github.io',
-    base: 'prometeo-root'
+    base: 'Prometeo-Root'
 });
     
